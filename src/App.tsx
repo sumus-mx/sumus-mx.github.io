6ca1c9ccd9802bfdbf5e9100fc1815723652245d
sumus <sumus.mx@gmail.com>
2024-01-18 01:36:26 -0600

    initial presentation

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Sumus mx</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
