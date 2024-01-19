a652f9383f22311dd541190ca886c5ce23e3673b
sumus <sumus.mx@gmail.com>
2024-01-18 20:59:20 -0600

    last demo build

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Sumus from './assets/sumus.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className='container'>
+        <a href="https://www.instagram.com/sumus.mx/" target="_blank" rel="noreferrer">
+          <img src={Sumus} alt="sumus" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Sumus mx</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
       </div>
     </>
   )
